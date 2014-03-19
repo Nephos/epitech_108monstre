40e0c48d0cc5232fbe0dd5d3b553942d1d24a09b
poulet_a <poulet_a@epitech.eu>
2014-03-19 15:37:34 +0100

    Fix lol

--- 108monstre.rb
+++ 108monstre.rb
@@ -5,7 +5,7 @@ require 'myerror'
 require_relative 'interactif.rb'
 require_relative 'main.rb'
 
-if ARGV.select{|s| s.to_s.match /(^\-h)|(^\-\-help)/}
+if ARGV.select{|s| s.to_s.match /(^\-h)|(^\-\-help)/}.size > 0
   exec("man ./108monstre.man")
 elsif ARGV.size == 1
   MyError::Error.call "Not enough arguments"
